fe682146f11f70956fa7b029c862306bd988dc34
jon-jh <hiebert456@gmail.com>
2025-12-13 12:30:39 -0600

    feat: add active class highlight for current open navigation link

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,36 +1,43 @@
-import styles from '../styles/NavBar.module.scss';
+"use client"; // required in Next.js App Router for hooks
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import styles from "../styles/NavBar.module.scss";
 
 const NavBar = () => {
-  const navigationLinks = [
-    'portfolio',
-    'e-commerce',
-    'blog',
-    'event'
-  ];
+  const pathname = usePathname();
+
+  const navigationLinks = ["portfolio", "e-commerce", "blog", "event"];
 
   return (
     <header className={styles.navbar}>
       <div className={styles.container}>
         {/* Logo */}
-        <a href="/" className={styles.logo}>
+        <Link href="/" className={styles.logo}>
           StylePress
-        </a>
+        </Link>
 
         {/* Navigation Links */}
         <nav className={styles.links}>
-          {navigationLinks.map((link, index) => (
-            <a
-              key={link}
-              href={`/${link}`}
-              className={`
-                ${index === 0 ? styles.first : ''}
-                ${index === navigationLinks.length - 1 ? styles.last : ''}
-                ${index !== navigationLinks.length - 1 ? styles.divider : ''}
-              `}
-            >
-              {link}
-            </a>
-          ))}
+          {navigationLinks.map((link, index) => {
+            const href = `/${link}`;
+            const isActive = pathname === href;
+
+            return (
+              <Link
+                key={link}
+                href={href}
+                className={`
+                  ${index === 0 ? styles.first : ""}
+                  ${index === navigationLinks.length - 1 ? styles.last : ""}
+                  ${index !== navigationLinks.length - 1 ? styles.divider : ""}
+                  ${isActive ? styles.active : ""}
+                `}
+              >
+                {link}
+              </Link>
+            );
+          })}
         </nav>
 
         {/* Profile / Avatar */}
